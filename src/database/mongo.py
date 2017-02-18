e071b525fb78c2634ea4b857f5e140738678a068
wakr <wakr@cs.helsinki.fi>
2017-02-18 13:55:29 +0200

    fix deps

--- src/database/mongo.py
+++ src/database/mongo.py
@@ -12,13 +12,14 @@ def getURL2(username, password):
 client = MongoClient(getURL2(DBUSER, DBPWD))
 db = client.dota2_large
 
+print(client)
 
 def getDocuments():
     result = []
     try:
         result = list(db.matches.find({}, {"players.hero_id": 1, "players.player_slot": 1,
                                            "players.account_id": 1, "radiant_win": 1})
-                                )
+                                .limit(1000))
     except Exception as e:
         print(e)
         return e
